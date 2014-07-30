c88391dc9b951387521347eaf44a9db632333539
dlsso <supertonesfan@hotmail.com>
2014-07-30 10:33:17 -0600

    Used Ajax to send random numbers to page

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', indexController.index);
+
+app.get('/numbers', function(req, res) {
+	res.send([
+		Math.floor(Math.random()*100),
+		Math.floor(Math.random()*100),
+		Math.floor(Math.random()*100),
+		Math.floor(Math.random()*100),
+		Math.floor(Math.random()*100)
+		])
+})
 
 var server = app.listen(3597, function() {
 	console.log('Express server listening on port ' + server.address().port);
---
+++ public/scripts/index.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	$('#random').on('click', function(e) {
+
+		e.preventDefault();
+
+		$.get('/numbers', function(numbers) {
+
+			$('#numbers').append(numbers.map(function(n) {
+				return $('<li>').text(n);
+			}))
+		})
+	})
+})
